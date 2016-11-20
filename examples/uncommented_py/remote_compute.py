09f6c257dfa5f05a846b3973f99675d4ba3845ad
Nicolas CELLIER <contact@nicolas-cellier.net>
2016-11-20 11:57:54 +0100

    quick commit

--- examples/uncommented_py/remote_compute.py
+++ examples/uncommented_py/remote_compute.py
@@ -21,7 +21,7 @@ formatter = logging.Formatter(
     '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 parameters = {}
 
