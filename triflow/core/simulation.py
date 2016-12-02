863e3d09333d12327a385b13f7b5de09b33b805e
Nicolas CELLIER <eryole@gmail.com>
2016-12-02 16:57:43 +0100

    asynchrone  tools for writing

--- triflow/core/simulation.py
+++ triflow/core/simulation.py
@@ -2,7 +2,7 @@
 # coding=utf8
 
 import asyncio
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 from threading import Lock
 import itertools as it
 import logging
@@ -19,7 +19,7 @@ class Simulation(object):
 
     def __init__(self, solver, U0, t0=0, id=None, **pars):
         self.simuloop = asyncio.new_event_loop()
-        self.executor = ProcessPoolExecutor(max_workers=1)
+        self.executor = ThreadPoolExecutor(max_workers=1)
 
         self.datalock = Lock()
         self.id = str(uuid1()) if id is None else id
