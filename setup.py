5ca893f9a3bed6c74adf33c352317db07686426c
Nicolas CELLIER <eryole@gmail.com>
2016-12-09 17:14:41 +0100

    upgrade setup version, code stable

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 setup(
     name='triflow',
     # la version du code
-    version="0.2.2",
+    version="0.2.3",
     # Liste les packages à insérer dans la distribution
     packages=find_packages(),
 
