80e35b1038cc3067ac7dcae8f097188cd768356f
Nicolas CELLIER <eryole@gmail.com>
2016-12-21 15:30:47 +0100

    branching for optimization

--- triflow/triflow_helper/analyse.py
+++ triflow/triflow_helper/analyse.py
@@ -4,18 +4,32 @@
 import logging
 
 import numpy as np
-from project_path import *
-from triflow.core.solver import Solver
+import toolz
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 logging = logging.getLogger(__name__)
 
 
-def load_data(sample_id):
+@toolz.curry
+def load_data(path, sample_id):
     """
         load simulation field result (not metadata),
         in a lazy way (see numpy npz file format)
     """
     logging.info('reading %s' % sample_id)
-    data = np.load(data_dir / 'runs' / sample_id / 'data.npz')
+    data = np.load(path / sample_id / 'data.npz')
     return data
+
+
+def compute_Nu(data, parameters, phi_flat=None):
+    """
+        load simulation field result (not metadata),
+        in a lazy way (see numpy npz file format)
+    """
+    if phi_flat is None:
+        return data['phi'] / parameters['phi_flat']
+    return data['phi'] / phi_flat
+
+def update_field(helpers, data, parameters):
+    data = dict(data)
+    data.update({})
