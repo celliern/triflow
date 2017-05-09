c83fd8bf0ee072216a22d2946f7de7fefc8ba345
Nicolas Cellier <contact@nicolas-cellier.net>
2017-05-09 15:42:57 +0200

    new release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 setup(
     name='triflow',
     # la version du code
-    version="0.3.2",
+    version="0.4.0",
     # Liste les packages à insérer dans la distribution
     packages=find_packages(),
 
@@ -46,7 +46,7 @@ setup(
         'bokeh': ["bokeh"],
         'datreant': ["datreant.core"],
     },
-    download_url='https://github.com/celliern/triflow/archive/0.3.2.tar.gz',
+    download_url='https://github.com/celliern/triflow/archive/0.4.0.tar.gz',
     # Il est d'usage de mettre quelques metadata à propos de sa lib
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers.
 
