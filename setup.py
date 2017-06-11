81f64e55c1bd40c5c132089571a701cc84daa1be
Nicolas CELLIER <eryole@gmail.com>
2017-06-11 17:11:01 +0200

    fix minor bug

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 setup(
     name='triflow',
     # la version du code
-    version="0.4.4",
+    version="0.4.5",
     # Liste les packages à insérer dans la distribution
     packages=find_packages(),
 
@@ -44,13 +44,12 @@ setup(
     extras_require={
         'bokeh': ["bokeh"],
     },
-    download_url='https://github.com/celliern/triflow/archive/v0.4.3.tar.gz',
+    download_url='https://github.com/celliern/triflow/archive/v0.4.5.tar.gz',
     # Il est d'usage de mettre quelques metadata à propos de sa lib
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers.
 
     classifiers=[
         "Programming Language :: Python",
-        "Development Status :: 3 - Alpha",
-        "License :: BEERWARE",
+        "Development Status :: 3 - Alpha"
     ],
 )
