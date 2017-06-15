65b484b33728425cfc143928d0060814c730043b
Nicolas Cellier <contact@nicolas-cellier.net>
2017-06-15 16:47:53 +0200

    fix bug with writeable fields.uflat

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 setup(
     name='triflow',
     # la version du code
-    version="0.4.5",
+    version="0.4.6",
     # Liste les packages à insérer dans la distribution
     packages=find_packages(),
 
@@ -44,7 +44,7 @@ setup(
     extras_require={
         'bokeh': ["bokeh"],
     },
-    download_url='https://github.com/celliern/triflow/archive/v0.4.5.tar.gz',
+    download_url='https://github.com/celliern/triflow/archive/v0.4.6.tar.gz',
     # Il est d'usage de mettre quelques metadata à propos de sa lib
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers.
 
