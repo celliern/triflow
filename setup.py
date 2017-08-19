868d9436476dbfea5149b8bc01b5fd11c6bda714
Nicolas Cellier <contact@nicolas-cellier.net>
2017-08-19 12:00:31 +0200

    typo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 setup(
     name='triflow',
     # la version du code
-    version="0.4.10",
+    version="0.4.11",
     # Liste les packages à insérer dans la distribution
     packages=find_packages(),
 
@@ -42,7 +42,7 @@ setup(
     extras_require={
         'bokeh': ["bokeh"],
     },
-    download_url='https://github.com/celliern/triflow/archive/v0.4.10.tar.gz',
+    download_url='https://github.com/celliern/triflow/archive/v0.4.11.tar.gz',
     # Il est d'usage de mettre quelques metadata à propos de sa lib
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers.
 
