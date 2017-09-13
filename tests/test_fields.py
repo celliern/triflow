79222ca94dcb03931b2e601b3df61717685d4cd6
Nicolas Cellier <contact@nicolas-cellier.net>
2017-09-13 19:58:11 +0200

    update fields tests

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -17,7 +17,7 @@ def test_fields_api():
     print(fields)
     assert np.isclose(fields.uflat,
                       np.vstack([U1, U2]).flatten('F')).all()
-    assert np.isclose(fields.flat,
-                      np.vstack([x, U1, U2, s]).flatten('F')).all()
-    for field, tested_field in zip(fields, np.vstack([x, U1, U2, s]).T):
-        assert np.isclose(field, tested_field).all()
+    assert np.isclose(fields["x"], x).all()
+    assert np.isclose(fields["U1"], U1).all()
+    assert np.isclose(fields["U2"], U2).all()
+    assert np.isclose(fields["s"], s).all()
