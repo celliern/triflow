f60508227680f921ddb30e078c97477247104d7f
Nicolas Cellier <contact@nicolas-cellier.net>
2018-04-27 15:48:31 +0200

    update test_display

--- tests/test_displays.py
+++ tests/test_displays.py
@@ -4,4 +4,36 @@
 import numpy as np
 import pytest
 
-from triflow import Model, Simulation
+from triflow import Model, Simulation, display_fields, display_probe
+
+
+@pytest.fixture
+def heat_model():
+    model = Model(differential_equations="k * dxxT",
+                  dependent_variables="T",
+                  parameters="k", compiler="numpy")
+    return model
+
+
+def test_display_fields(heat_model):
+    x, dx = np.linspace(0, 10, 50, retstep=True, endpoint=False)
+    T = np.cos(x * 2 * np.pi / 10)
+    initial_fields = heat_model.fields_template(x=x, T=T)
+    parameters = dict(periodic=True, k=1)
+    simul = Simulation(heat_model, initial_fields, parameters,
+                       dt=1, tmax=2, tol=1E-1)
+    display_fields(simul)
+    for i, (t, fields) in enumerate(simul):
+        continue
+
+
+def test_display_probes(heat_model):
+    x, dx = np.linspace(0, 10, 50, retstep=True, endpoint=False)
+    T = np.cos(x * 2 * np.pi / 10)
+    initial_fields = heat_model.fields_template(x=x, T=T)
+    parameters = dict(periodic=True, k=1)
+    simul = Simulation(heat_model, initial_fields, parameters,
+                       dt=1, tmax=2, tol=1E-1)
+    display_probe(simul, function=lambda simul: simul.timer.total)
+    for i, (t, fields) in enumerate(simul):
+        continue
