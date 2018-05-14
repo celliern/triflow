d4b49a0236d65bdb3b651f053d66862c2ab8e525
Nicolas CELLIER <eryole@gmail.com>
2018-05-14 17:37:45 +0200

    fix error in pickle test

--- tests/test_pickle.py
+++ tests/test_pickle.py
@@ -21,9 +21,9 @@ def test_pickling(heat_model):
     T = np.cos(x * 2 * np.pi / 10)
     initial_fields = heat_model.fields_template(x=x, T=T)
     parameters = dict(periodic=True, k=1)
-    simul = enumerate(Simulation(heat_model, initial_fields,
-                                 parameters, dt=1, tmax=100,
-                                 tol=1E-1))
+    simul = Simulation(heat_model, initial_fields,
+                       parameters, dt=1, tmax=100,
+                       tol=1E-1)
     container = simul.attach_container("/tmp/test")
 
     pickle.loads(pickle.dumps(heat_model))
